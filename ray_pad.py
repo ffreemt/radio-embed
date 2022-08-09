930380d742e51aeb504a53600d3c911899239d32
freemt <yucongo+fmt@gmail.com>
2022-08-10 00:49:47 +0800

    Update

--- ray_pad.py
+++ ray_pad.py
@@ -10,16 +10,23 @@ from pathlib import Path
 # import joblib
 import more_itertools as mit
 import numpy as np
+import psutil
 import ray
 from about_time import about_time
 from logzero import logger
 
 from radio_embed import radio_embed
 
-num_cpus = multiprocessing.cpu_count()
-filename = "/home/mu2018/github/en-de-zh-txt/fangfang-en.txt"
+num_cpus_m = multiprocessing.cpu_count()
+num_cpus = psutil.cpu_count(logical=False)
+
+filename = "fangfang-en.txt"
 lines = Path(filename).read_text("utf8").splitlines()
 lst = [_.strip() for _ in lines if _.strip()]
+
+args = "\n".join(lst)
+args_m = ["\n".join(elm) for elm in mit.divide(num_cpus_m, lst)]
+args = ["\n".join(elm) for elm in mit.divide(num_cpus, lst)]
 
 # with about_time() as dur: res1 = radio_embed("\n".join(lst))
 # 143.72 s
@@ -28,21 +35,20 @@ lst = [_.strip() for _ in lines if _.strip()]
 
 def test_pool(func, args_):
     """Test."""
-    with Pool(num_cpus) as pool:
+    with Pool(num_cpus_m) as pool:
         ret = pool.map(func, args_)
     # pool.close()
     # pool.join()
     return ret
 
-
-args = "\n".join(lst)
-args = ["\n".join(elm) for elm in mit.divide(num_cpus, lst)]
-# with about_time() as dur2: res2 = test_pool(radio_embed, args)
+# with about_time() as dur2: res2 = test_pool(radio_embed, args_m)
 # print(dur2.duration)
-# 26.5s  about 1/6 of
+# forindo 26.5s  about 1/6 of 140s
+# okteto 256 s! > 210s!
 
 # res2a = np.concatenate(res2)
 # np.allclose(res1, res2a, rtol=1e-05, atol=1e-07)
+# np.allclose(res1, np.concatenate(res2), rtol=1e-05, atol=1e-07)
 
 # %timeit ret = joblib.Parallel(n_jobs=num_cpus, backend='loky', verbose=0)(joblib.delayed(radio_embed)(arg) for arg in args)
 # 28.1 s ± 1.08 s per loop (mean ± std. dev. of 7 runs, 1 loop each)
@@ -69,16 +75,18 @@ def main():
     with about_time() as dur:
         res1 = radio_embed("\n".join(lst))
     print(dur.duration_human)
-    # 143.72 s 137 s
+    # forindo 143.72 s 137 s
+    # okteto 3:33 (210s)
     # """
 
     with about_time() as dur5:
         _ = [ray_embed.remote(arg) for arg in args]
         res5 = ray.get(_)
-    print(num_cpus, dur5.duration_human)  # 40s
-    res5a = np.concatenate(res5)
+    print(num_cpus, dur5.duration_human)  # forindo 40s
+    # res5a = np.concatenate(res5)
     # _ = np.allclose(res1, res5a, rtol=1e-05, atol=1e-07)
-    # print(_)
+    _ = np.allclose(res1, np.concatenate(res5), rtol=1e-05, atol=1e-07)
+    print(_)
 
     ray.shutdown()
     ray.init(num_cpus=num_cpus // 2)
